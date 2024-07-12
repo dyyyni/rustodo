398360c7048edec18e52766155031c613a0615af
Daniel Luoma <luomadaniel95@gmail.com>
2024-07-12 15:05:28 +0300

    [Tutorials] add 'hello world' example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod app;
+use crate::app::Rustodo;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    let _ = eframe::run_native("My egui App", native_options,
+     Box::new(|cc| Ok(Box::new(Rustodo::new(cc)))));
 }
+
