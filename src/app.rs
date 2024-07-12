1b083601c1b957a34ada634b4e51b3aa86822242
Daniel Luoma <luomadaniel95@gmail.com>
2024-07-12 15:19:59 +0300

    [Tutorials] testing more example code

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,8 @@
 #[derive(Default)]
-pub struct Rustodo {}
+pub struct Rustodo {
+    name: String,
+    age: u32,
+}
 
 impl Rustodo {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
@@ -7,7 +10,10 @@ impl Rustodo {
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        Self::default()
+        Self {
+            name: String::new(),
+            age: 0,
+        }
     }
 }
 
@@ -15,6 +21,15 @@ impl eframe::App for Rustodo {
    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
            ui.heading("Hello World!");
+           ui.horizontal(|ui| {
+            ui.label("Your name: ");
+            ui.text_edit_singleline(&mut self.name);
+           });
+           ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+           if ui.button("Increment").clicked() {
+            self.age += 1;
+           }
+           ui.label(format!("Hello {}, age {}", self.name, self.age));
        });
    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::app::Rustodo;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    let _ = eframe::run_native("My egui App", native_options,
+    let _ = eframe::run_native("Rustodo", native_options,
      Box::new(|cc| Ok(Box::new(Rustodo::new(cc)))));
 }
 
